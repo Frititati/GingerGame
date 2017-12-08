c46e844e32596c1bee748dce74516ecbd96a621a
filippomc <filippo1mc@gmail.com>
2017-12-08 12:40:36 +0100

    end of connections

--- src/mainserver/Connection_Manager.java
+++ src/mainserver/Connection_Manager.java
@@ -9,12 +9,11 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
-import mainclient.Client_Run;
-
 public class Connection_Manager extends Thread {
   public InetAddress[] clients_IPs = new InetAddress[2];
   private UUID[] clients_UUID = new UUID[2];
   private DatagramSocket server_socket;
+  private int client_turn = 0;
 
   public Connection_Manager() {
     this.start();
@@ -48,7 +47,7 @@ public class Connection_Manager extends Thread {
           add_map(map_vars, request_IP);
           break;
         case "turnakk":
-
+          check_movement_validity(map_vars, request_IP);
           break;
         default:
           System.out.println("filtered wrong request");
@@ -57,6 +56,72 @@ public class Connection_Manager extends Thread {
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private void check_movement_validity(Map vars, InetAddress request_IP) {
+    UUID UUID_temp = UUID.fromString((String) vars.get("UUID"));
+    int y = Integer.parseInt((String) vars.get("ypos"));
+    int x = Integer.parseInt((String) vars.get("xpos"));
+    int client_num = -1;
+    boolean error = false;
+    if (UUID_temp.equals(clients_UUID[0])) {
+      client_num = 0;
+    } else if (UUID_temp.equals(clients_UUID[1])) {
+      client_num = 1;
+    } else {
+      error = true;
+    }
+    int validity_retval = CDS.check_move(client_num, x, y);
+    int other_client = -1;
+    if (client_turn == 0) {
+      other_client = 1;
+    } else {
+      other_client = 0;
+    }
+    switch (validity_retval) {
+    case -1:
+      send_game_end(client_turn, other_client);
+      break;
+    case 1:
+      send_game_end(other_client, client_turn);
+      break;
+    case 0:
+      send_treasure();
+    case 2:
+      send_movement(x, y);
+      break;
+    }
+  }
+
+  private void send_movement(int x, int y) {
+    String[] keys = { "id", "xpos", "ypos" };
+    String[] values = { client_turn + "", x + "", y + "" };
+    byte[] send_movement_byte = create_packet_bytes("movebroad", keys, values);
+    send_packets(send_movement_byte, clients_IPs[0]);
+    send_packets(send_movement_byte, clients_IPs[1]);
+    if (client_turn == 0) {
+      client_turn = 1;
+    } else {
+      client_turn = 0;
+    }
+    turn_ask();
+  }
+
+  private void send_treasure() {
+    String[] keys = { "id" };
+    String[] values = { client_turn + "" };
+    byte[] send_treasure_byte = create_packet_bytes("treasure", keys, values);
+    send_packets(send_treasure_byte, clients_IPs[client_turn]);
+  }
+
+  private void send_game_end(int winner, int loser) {
+    String[] keys = { "winner", "loser", "status" };
+    String[] values = { winner + "", loser + "", "2" };
+    byte[] send_end_byte = create_packet_bytes("endgame", keys, values);
+    send_packets(send_end_byte, clients_IPs[0]);
+    send_packets(send_end_byte, clients_IPs[1]);
+    CDS.clients_status[0] = 2;
+    CDS.clients_status[1] = 2;
   }
 
   private void add_map(Map vars, InetAddress request_IP) {
@@ -111,6 +176,10 @@ public class Connection_Manager extends Thread {
     } catch (ArrayIndexOutOfBoundsException e) {
       if (CDS.check_sent_map_status()) {
         CDS.setup_map();
+        send_maps();
+        CDS.clients_status[0] = 3;
+        CDS.clients_status[1] = 3;
+        turn_ask();
       }
     }
 
@@ -122,6 +191,21 @@ public class Connection_Manager extends Thread {
     } else {
       send_error(client_num, "The UUID is not correct and you are suspicious");
     }
+  }
+
+  private void send_maps() {
+    String[] keys = { "map" };
+    String[] values = { CDS.merge_sent_maps() };
+    byte[] send_map_byte = create_packet_bytes("fullmap", keys, values);
+    send_packets(send_map_byte, clients_IPs[0]);
+    send_packets(send_map_byte, clients_IPs[1]);
+  }
+
+  private void turn_ask() {
+    String[] keys = { "UUID" };
+    String[] values = { clients_UUID[client_turn].toString() };
+    byte[] turn_ask_byte = create_packet_bytes("turnask", keys, values);
+    send_packets(turn_ask_byte, clients_IPs[client_turn]);
   }
 
   public void connection_request(Map vars, InetAddress client_ip) {
